20dc8d084da9616f3600949090698ac1a6b3f10b
vskalozub@marqeta.com <vskalozub@marqeta.com>
2023-03-12 19:45:59 +0400

    revert url changes

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -16,7 +16,7 @@ const useProducts = () => {
     async function fetchProducts() {
       try {
         setLoading(true);
-        const response = await fetch(process.env.REACT_APP_NETLIFY_FUNCTION_URL)
+        const response = await fetch('http://localhost:9999/.netlify/functions/products')
         const products = await response.json();
         setProducts(products);
       } catch(error) {
